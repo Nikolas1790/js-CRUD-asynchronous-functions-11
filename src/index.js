7ef4eafe0d13e6d25c1be7b1af1b2d2a82d8f2e3
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-07-18 10:31:27 +0300

    markup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Notiflix from 'notiflix';
+import axios from 'axios';
+axios.defaults.headers.common["x-api-key"] = "";
+
+// let api = axios.create({
+//     baseURL: 'https://api.thecatapi.com/v1/breeds'
+// });
